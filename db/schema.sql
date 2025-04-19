52d22ee77309415097ca5846f40227409c9cecfa
h7mmxd <hammadaliatif@gmail.com>
2025-04-19 11:58:21 +0300

    Updated user table to include isAdmin

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,13 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT false
 );
+
+-- Execude the below command if you already have a table created and want to add just the admin column
+-- ALTER TABLE users ADD COLUMN is_admin BOOLEAN DEFAULT false;
+
+
 
 CREATE TABLE calculations (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -29,10 +35,10 @@ CREATE TABLE feedback (
     user_id INT,
     topic VARCHAR(100) DEFAULT 'General',
     message TEXT NOT NULL,
-    isABug BOOLEAN FALSE,
+    isABug BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     resolved BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    
 );
+
